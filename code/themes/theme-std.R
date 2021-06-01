b53ff1d5feae032f6ce10839ae1eab101dc524ea
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2021-06-01 21:24:26 +0200

    updated themes

--- code/themes/theme-std.R
+++ code/themes/theme-std.R
@@ -14,19 +14,19 @@ theme_std <- theme(
   panel.grid.major=element_line(colour="grey"), #Major and minor gridlines
   panel.grid.minor=element_line(colour="white"), 
   panel.spacing = unit(1, "lines"),
-  plot.title = element_text(hjust = 0.5, vjust=0, size=12, face="bold"), #Plot title, thought don't tend to use
-  text=element_text(size=9), #General text 
-  axis.title.y = element_text(size=14, margin = margin(t = 0, r = 10, b = 0, l = 0)), #Axis labels
-  axis.title.x = element_text(size=14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
-  axis.text.x = element_text(size=11, margin = margin(t = 4, r=0, b=0, l=0), colour="black"), #Axis text
-  axis.text.y = element_text(size=11, margin = margin(t = 0, r=4, b=0, l=0), colour="black"),
+  plot.title = element_text(family = "avenir-book", hjust = 0.5, vjust=0, size=12, face="bold"), #Plot title, thought don't tend to use
+  text=element_text(family = "avenir-book", size=9), #General text 
+  axis.title.y = element_text(family = "avenir-book", size=14, margin = margin(t = 0, r = 10, b = 0, l = 0)), #Axis labels
+  axis.title.x = element_text(family = "avenir-book", size=14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+  axis.text.x = element_text(family = "avenir-book", size=11, margin = margin(t = 4, r=0, b=0, l=0), colour="black"), #Axis text
+  axis.text.y = element_text(family = "avenir-book", size=11, margin = margin(t = 0, r=4, b=0, l=0), colour="black"),
   axis.ticks.length=unit(0.3, "cm"),
   axis.ticks = element_line(colour = "grey"),
-  strip.text.x = element_text(size = 8, face = "bold"),
+  strip.text.x = element_text(family = "avenir-book", size = 8, face = "bold"),
   strip.background = element_blank(),
   legend.background= element_rect(fill=scales::alpha("#CCCCCC", 0.03)), #Legend background colour
-  legend.title=element_text(size=8, face="bold"), #Legend title
-  legend.text=element_text(size=8), #Legend text
+  legend.title=element_text(family = "avenir-book", size=8, face="bold"), #Legend title
+  legend.text=element_text(family = "avenir-book", size=8), #Legend text
   legend.position="right", #Legend position
   legend.direction="vertical", #Legend stacking
   legend.justification = "left", #Left justify legend
