36ba3824092ce7ffba64c55c3b248ba06c044ce4
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2021-07-19 11:15:01 +0200

    Added sub cohorts

--- code/themes/lc-names-neat.R
+++ code/themes/lc-names-neat.R
@@ -5,15 +5,16 @@
 ## Author: Tim Cadman
 ## Email: t.cadman@bristol.ac.uk
 ################################################################################
+library(tidyverse)
 
 names_neat <- tibble(
   cohort = c(
-    "alspac", "bib", "chop", "dnbc", "eden", "elfe", "gecko", "genr", "hgs", 
-    "inma", "moba", "ninfea", "nfbc66", "nfbc86", "raine", "rhea", "sws", 
-    "combined"),
+    "alspac", "bib", "chop", "dnbc", "eden", "eden_nan", "eden_poit", "elfe", 
+    "gecko", "genr", "hgs", "inma", "inma_gip", "inma_sab", "inma_val", "moba", 
+    "ninfea", "nfbc66", "nfbc86", "raine", "rhea", "sws", "combined"),
   cohort_neat = c(
-    "ALSPAC", "BiB", "CHOP", "DNBC", "EDEN", "ELFE", "GECKO", "GENR", "HGS", 
-    "INMA", "MoBa", "NINFEA", "NFBC66", "NFBC86", "Raine", "RHEA", "SWS", 
+    "ALSPAC", "BiB", "CHOP", "DNBC", "EDEN", "EDEN NANCY", "EDEN POITIERS", 
+    "ELFE", "GECKO", "GENR", "HGS", "INMA", "INMA Gipuzkoa", "INMA Sabadell", 
+    "INMA Valencia", "MoBa", "NINFEA", "NFBC66", "NFBC86", "Raine", "RHEA", "SWS", 
     "Pooled results")
   )
-  
