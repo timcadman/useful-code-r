8402caf919d06bf559187ab4ea78f1ec8db7edce
Tim Cadman <timcadman@gmail.com>
2022-10-10 09:08:08 +0000

    added abcd

--- code/themes/lc-names-neat.R
+++ code/themes/lc-names-neat.R
@@ -9,11 +9,11 @@ library(tibble)
 
 names_neat <- tibble(
   cohort = c(
-    "alspac", "bib", "chop", "dnbc", "eden", "eden_nan", "eden_poit", "elfe", 
+    "abcd", "alspac", "bib", "chop", "dnbc", "eden", "eden_nan", "eden_poit", "elfe", 
     "gecko", "genr", "hgs", "inma", "inma_gip", "inma_sab", "inma_val", "moba", 
     "ninfea", "nfbc66", "nfbc86", "raine", "rhea", "sws", "combined"),
   cohort_neat = c(
-    "ALSPAC", "BiB", "CHOP", "DNBC", "EDEN", "EDEN Nancy", "EDEN Poitiers", 
+    "ABCD", "ALSPAC", "BiB", "CHOP", "DNBC", "EDEN", "EDEN Nancy", "EDEN Poitiers", 
     "ELFE", "GECKO", "GENR", "HGS", "INMA", "INMA Gipuzkoa", "INMA Sabadell", 
     "INMA Valencia", "MoBa", "NINFEA", "NFBC66", "NFBC86", "Raine", "RHEA", "SWS", 
     "Combined"))
